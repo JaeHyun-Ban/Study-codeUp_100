8bd700f567815c27e5e5da25ca59404e9a128253
JaeHyun-Ban <wogus0008@gmail.com>
2021-05-14 18:21:21 +0900

    꾸준히

---
+++ TestJava/src/Basic21_39/test22.java
@@ -0,0 +1,17 @@
+package Basic21_39;
+
+import java.util.Scanner;
+
+// 1022 : [기초-입출력] 문장 1개 입력받아 그대로 출력하기(설명)
+// 공백 문자가 포함되어 있는 문장을 입력받고 그대로 출력하는 연습을 해보자.
+
+public class test22 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // nextLine()는 엔터 이전의 "공백을 포함한 문자열"을 받아준다
+    String str = sc.nextLine();
+
+    System.out.println(str);
+  }
+}
